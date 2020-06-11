e67a35450cf9c9c3c22ccad4a203f8ef98c06558
Anthony <a.a.donovan@outlook.com>
2020-06-11 13:40:16 +0100

    Complete exercises:

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-console.log('hello!')
-
+console.log('1')
 $.get("https://async-workshops-api.herokuapp.com/people", function(peopleResponse) {
-   $("#person").html(peopleResponse[0].name);
+  console.log(peopleResponse);
+  peopleResponse.forEach(function(person) {
+    console.log(person.name);
+    console.log('3')
+  });
 });
+console.log('2')
 
+// $.ajax({
+//   type: 'GET',
+//   url: "https://async-workshops-api.herokuapp.com/people",
+//   success: function(data) {
+//     console.log("People response:", data);
+//   }
+// });
